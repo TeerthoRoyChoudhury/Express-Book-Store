37c86ca6b765cc45fbc7eb5a262505a8d0923f1e
Teertho Roychoudhury <roychoudhuryteertho96@gmail.com>
2026-04-17 14:15:21 +0530

    feat: implement book routes and middleware for handling book operations

---
+++ db/book.js
@@ -0,0 +1,12 @@
+exports.BOOKS = [
+  {
+    id: 1,
+    title: "Book One",
+    author: "Author One",
+  },
+  {
+    id: 2,
+    title: "Book two",
+    author: "Author Two",
+  },
+];
--- index.js
+++ index.js
@@ -1,84 +1,17 @@
 const express = require("express");
 const fs = require("node:fs");
 const app = express();
+const bookRouter = require("./routes/book.routes");
 const port = 8000;
-
-// IN MEMORY DATABASE
-const books = [
-  {
-    id: 1,
-    title: "Book One",
-    author: "Author One",
-  },
-  {
-    id: 2,
-    title: "Book two",
-    author: "Author Two",
-  },
-];
+const { logger_middleware } = require("./middleware/logger");
 
 // Middlewares (Plugins)
 app.use(express.json());
 
-function logger_middleware(req, res, next) {
-  const log = `\n[${Date.now()} ${req.method} ${req.path}]`;
-  fs.appendFileSync("log.txt", log, "utf-8");
-  next();
-}
-
-function custom_middleware(req, res, next) {
-  console.log(`Custom Middleware running`);
-  next();
-}
-
 app.use(logger_middleware);
 
 // Routes
-app.get("/books", (req, res) => {
-  res.setHeader("x-piy", "TeerthoRoyCh");
-  res.json(books);
-});
-
-app.get("/books/:id", custom_middleware, (req, res) => {
-  const id = parseInt(req.params.id);
-  if (isNaN(id)) {
-    return res.status(400).json({ error: `Id must be of type INT` });
-  }
-  const book = books.find((e) => e.id === id);
-
-  if (!book) {
-    return res.status(404).json({ error: `Book with ${id} does not exist` });
-  }
-  return res.json(book);
-});
-
-app.post("/books", (req, res) => {
-  const { title, author } = req.body;
-  if (!title || title === "") {
-    return res.status(400).json({ error: "title is required" });
-  }
-  if (!author || author === "") {
-    return res.status(400).json({ error: `Author name is required` });
-  }
-
-  const book = { id: books.length + 1, title, author };
-  books.push(book);
-
-  return res.status(201).json({ message: `Book created success`, id: book.id });
-});
-
-app.delete("/books/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  if (isNaN(id)) {
-    return res.status(400).end("Id should be an integer");
-  }
-  const booktodel = books.findIndex((num) => num.id === id);
-  if (booktodel < 0) {
-    return res.status(404).json({ error: `Book with id ${id} does not exist` });
-  }
-  books.splice(booktodel, 1);
-  return res.status(200).json({ message: "Book deleted" });
-});
+app.use("/books", bookRouter);
 
 app.listen(port, () => {
   console.log(`HTTP is running on PORT ${port}`);
