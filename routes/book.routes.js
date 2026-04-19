67e55f7f484ed63c901efcdfa4010a4aaae0dfd8
Teertho Roychoudhury <roychoudhuryteertho96@gmail.com>
2026-04-19 11:31:11 +0530

    feat: implement book management functionality with controller and model separation

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,51 +1,13 @@
 const express = require("express");
-const { BOOKS } = require("../db/book");
+const fromcontroller = require("../controller/rootcontroller");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.setHeader("x-piy", "TeerthoRoyCh");
-  res.json(BOOKS);
-});
+router.get("/", fromcontroller.showall);
 
-router.get("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  if (isNaN(id)) {
-    return res.status(400).json({ error: `Id must be of type INT` });
-  }
-  const book = BOOKS.find((e) => e.id === id);
+router.get("/:id", fromcontroller.pickid);
 
-  if (!book) {
-    return res.status(404).json({ error: `Book with ${id} does not exist` });
-  }
-  return res.json(book);
-});
+router.post("/", fromcontroller.addit);
 
-router.post("/", (req, res) => {
-  const { title, author } = req.body;
-  if (!title || title === "") {
-    return res.status(400).json({ error: "title is required" });
-  }
-  if (!author || author === "") {
-    return res.status(400).json({ error: `Author name is required` });
-  }
-
-  const book = { id: BOOKS.length + 1, title, author };
-  BOOKS.push(book);
-
-  return res.status(201).json({ message: `Book created success`, id: book.id });
-});
-
-router.delete("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  if (isNaN(id)) {
-    return res.status(400).end("Id should be an integer");
-  }
-  const booktodel = BOOKS.findIndex((num) => num.id === id);
-  if (booktodel < 0) {
-    return res.status(404).json({ error: `Book with id ${id} does not exist` });
-  }
-  BOOKS.splice(booktodel, 1);
-  return res.status(200).json({ message: "Book deleted" });
-});
+router.delete("/:id", fromcontroller.delit);
 
 module.exports = router;
